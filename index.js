ee405780dd934daf6d5d06387c14561d6707f170
Ryan Namgung <heon.namgung@richmond.edu>
2018-05-20 03:27:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,18 +5,16 @@ function myAnimal() {
 }
 
 function yourAnimal() {
-  animal = "cat"
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
+  animal = "cat"
   return animal
 }
 
 function add2(n) {
-  return n + two
-
-  // Feel free to move things around!
   const two = 2
+  return n + two
 }
 
 var funkyFunction = function() {
